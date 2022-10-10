8aaa0e9a8f243c1c7f781b7064168cabfaf13e5d
Nailson <nailsonlima131@gmail.com>
2022-10-10 18:46:16 -0300

    climaCidades

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,28 @@ const apiCountryURL = "https://countryflagsapi.com/png/";
 const cityInput = document.querySelector('#city-input');
 const searchBtn = document.querySelector('#search');
 
+const cityElement = document.querySelector('#city')
+const tempElement = document.querySelector('#temperature span')
+const descElement = document.querySelector('#description')
+const weatherElement = document.querySelector('#weather-icon')
+const countryElement = document.querySelector('#country')
+const umidityElement = document.querySelector('#umidity span')
+const windElement = document.querySelector('#wind span')
+
 // FUNÇÕES
+const getWeatherData = async(city) =>{
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    console.log(data)
+}
+
+
 const showWeaherData = (city) =>{
 
-    console.log(city)
+    getWeatherData(city)
 
 }
 
