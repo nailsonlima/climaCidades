71bf91dcf1ea7cad75d3ae44eda44a3a5d9830aa
Nailson <nailsonlima131@gmail.com>
2022-10-10 18:23:38 -0300

    climaCidades

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
 // 9b24105f78188588a9ed3ff6ea92d5ef key wather
+const apiKey = "9b24105f78188588a9ed3ff6ea92d5ef";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#search');
+
+// FUNÇÕES
+const showWeaherData = (city) =>{
+
+    console.log(city)
+
+}
+
+
+// EVENTOS
+searchBtn.addEventListener("click", (e)=>{
+    e.preventDefault()
+
+    const city = cityInput.value
+
+    showWeaherData(city)
+
+})
